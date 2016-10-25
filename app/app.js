d5ef700ef55fc08963d17e6534dd0b3b3b877172
SamGinzburg <ginzburg.sam@gmail.com>
2016-10-25 18:48:09 -0400

    fb4

--- app/app.js
+++ app/app.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Feed from './components/feed';
+import { getFeedData } from './server';
+
 ReactDOM.render(<Feed />, document.getElementById('fb-feed'));
